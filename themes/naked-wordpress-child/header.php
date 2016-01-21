b1b152d9a686242eb2a85a5b73de1cc9d557cfb5
Deepti Boddapati <deeptiboddapati@gmail.com>
2016-01-21 08:36:51 -0600

    pushing initial files

---
+++ themes/naked-wordpress-child/header.php
@@ -0,0 +1,142 @@
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* This template will be called by all other template files to begin 
+	/* rendering the page and display the header/nav
+	/*-----------------------------------------------------------------------------------*/
+?>
+<!DOCTYPE html <?php language_attributes(); ?>>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title>
+	<?php bloginfo('name'); // show the blog name, from settings ?> | 
+	<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page ?>
+</title>
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
+	// so if you want to load other stylesheets,
+	// I would load them with an @import call in your style.css
+?>
+
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+
+<?php wp_head(); 
+// This fxn allows plugins, and Wordpress itself, to insert themselves/scripts/css/files
+// (right here) into the head of your website. 
+// Removing this fxn call will disable all kinds of plugins and Wordpress default insertions. 
+// Move it if you like, but I would keep it around.
+?>
+
+</head>
+
+<body class="container-fluid" <?php body_class(); 
+	// This will display a class specific to whatever is being loaded by Wordpress
+	// i.e. on a home page, it will return [class="home"]
+	// on a single post, it will return [class="single postid-{ID}"]
+	// and the list goes on. Look it up if you want more.
+	?>> 
+  <div class="wrapper row ">
+    <header id="masthead" class= "col-md-3 col-md-offset-2">
+      <div role="navigation" data-nav-status='toggle' class="hidden">
+        <ul class="nav nav-pills nav-stacked navbar navbar-default navbar-fixed-top visible-xs ">
+          <li> <a class="btn-default" href="#navtop"  type="button" data-toggle="collapse" data-target="#navtop" aria-expanded="false" aria-controls="navtop"> Thoughts about Techsupport</a></li>
+          <div class="collapse" id="navtop">
+
+            <ul class=" nav nav-pills nav-stacked">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </div>
+          
+        </ul>
+      </div>
+      <div class="sidebar">
+      <h1>Wabi Sabi Programming</h1>
+      <div>
+        <div id="sociallinks"><span class="glyphicon icon-g-github"></span><span class="glyphicon icon-g-github"></span></div>
+      </div>
+      <div id="catchphrase" class="nomobile">
+        <code>>Dee.cool()<br />
+        <span class="output">False</span><br />
+        >Dee.passion()<br />
+        <span class="output">True</span></code>
+        <p></p>
+      </div>
+   <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+    
+  <div class="panel">
+      <a role="button" class="btn-default" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    <div  id="headingOne">
+    
+       
+          Menuitem1
+        
+     
+    </div>
+    </a>
+    <div id="collapseOne" class="panel-collapse collapse " role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+        <ul class=" nav nav-pills nav-stacked">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+    </div>
+      </div>
+  </div>
+  <div class="panel ">
+    <a class="collapsed btn-default" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+    <div id="headingTwo">
+      
+        
+          Menuitem2
+       
+     
+    </div>
+     </a>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        <ul class=" nav nav-pills nav-stacked">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+     </div>
+    </div>
+  </div>
+  <div class="panel ">
+    <a class="collapsed btn-default " role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+    <div  id="headingThree">     
+          Menuitem3
+    </div>
+    </a>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+           <ul class=" nav nav-pills nav-stacked">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </header>
---
+++ themes/naked-wordpress-child/index.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying the home/index page.
+ * This template will also be called in any case where the Wordpress engine 
+ * doesn't know which template to use (e.g. 404 error)
+ */
+
+get_header(); // This fxn gets the header.php file and renders it ?>
+
+	 <section class="col-md-6">
+      <div>
+        <h3>Nice to meet you, I'm Deepti.  </h3>
+        <p>I started this blog because <a href="http://www.joelonsoftware.com/articles/CollegeAdvice.html">Joel</a> told me to learn how to write. I stole the name from the <a href="http://c2.com/cgi/wiki/wiki?WabiSabi">c2 Wiki</a>. My personal mission is plugging cognitive leaks so that people can do more awesome. I'll use this chunk of the web to talk about my code, my mission, my favorite tools, and my favorite books.</p>
+      </div>
+      <div class="firstsec">
+        <h2>Projects</h2> </div>
+      <div class="projects">
+        <div class="row">
+          <div  class="col-md-4">
+           <a href="http://www.billtracker.org"> 
+          <image src='logo-balloon copy.png'/>
+        </a>
+        </div>
+          <div class="col-md-8">
+          <p>
+            I got the idea for BillTracker.org at an ATX OpenHack panel about how to bring tech into government. One of the panelists talked about how difficult following a bill through congress was. She likened it to watching a ping pong match. When ATX Hack For Change came around I recurited a team, wrote a pitch and planned out sprints with Trello. Having such a passionate team was a humbling and amazing experience. <ul class="collapse" id="readmore1"> Built with: <li> Django </li> <li> Annotator.js </li> </ul><a type="button" class="readmore" data-toggle="collapse" href="#readmore1" data-target="" aria-expanded="true" aria-controls="readmore1">See More</a></p>
+        </div>
+      </div>
+
+     <div class="row">
+          <image src='http://placehold.it/230x200' class="col-md-6"/>
+          <div class="col-md-6">
+          <p>
+            Lorem ipsum dolor sit amet, te fuisset commune cum. Ut mea salutatus scriptorem, no dolores abhorreant eos, sumo graecis ancillae mei ei. Mei ad dicit impetus periculis. Justo suavitate his ne, vis et brute singulis rationibus. Ei stet aliquip nec, annec quem percipit, cu mel lorem civibus offendit. Per nisl quaeque in, qui sumo quando urbanitas ne. <p class="collapse" id="readmore1">Et tation tamquam voluptatibus usu, admodum inciderint vis ad. Id quis magna mandamus qui, cu vel iudico ridens. Mundi malorum torquatos duo
+            et, mea an inani partem cotidieque. Vis nominavi detraxit no, et vix tale labore. Posse vivendum deserunt has in, quot.</p><a type="button" class="readmore" data-toggle="collapse" href="#readmore1" data-target="" aria-expanded="true" aria-controls="readmore1">See More</a></p>
+        </div>
+      </div>
+
+   <div class="firstsec">
+        <h2>Projects</h2> </div>
+      <div class="projects">
+        <div class="row">
+          <image src='http://placehold.it/230x200' class="col-md-6"/>
+          <div class="col-md-6">
+          <p>
+            Lorem ipsum dolor sit amet, te fuisset commune cum. Ut mea salutatus scriptorem, no dolores abhorreant eos, sumo graecis ancillae mei ei. Mei ad dicit impetus periculis. Justo suavitate his ne, vis et brute singulis rationibus. Ei stet aliquip nec, annec quem percipit, cu mel lorem civibus offendit. Per nisl quaeque in, qui sumo quando urbanitas ne. <p class="collapse" id="readmore1">Et tation tamquam voluptatibus usu, admodum inciderint vis ad. Id quis magna mandamus qui, cu vel iudico ridens. Mundi malorum torquatos duo
+            et, mea an inani partem cotidieque. Vis nominavi detraxit no, et vix tale labore. Posse vivendum deserunt has in, quot.</p><a type="button" class="readmore" data-toggle="collapse" href="#readmore1" data-target="#readmore1" aria-expanded="true" aria-controls="readmore1">See More</a></p>
+        </div>
+      </div>
+
+       <div class="row">
+          <image src='http://placehold.it/230x200' class="col-md-6"/>
+          <div class="col-md-6">
+          <p>
+            Lorem ipsum dolor sit amet, te fuisset commune cum. Ut mea salutatus scriptorem, no dolores abhorreant eos, sumo graecis ancillae mei ei. Mei ad dicit impetus periculis. Justo suavitate his ne, vis et brute singulis rationibus. Ei stet aliquip nec, annec quem percipit, cu mel lorem civibus offendit. Per nisl quaeque in, qui sumo quando urbanitas ne. <p class="collapse" id="readmore1">Et tation tamquam voluptatibus usu, admodum inciderint vis ad. Id quis magna mandamus qui, cu vel iudico ridens. Mundi malorum torquatos duo
+            et, mea an inani partem cotidieque. Vis nominavi detraxit no, et vix tale labore. Posse vivendum deserunt has in, quot.</p><a type="button" class="readmore" data-toggle="collapse" href="#readmore1" data-target="#readmore1" aria-expanded="true" aria-controls="readmore1">See More</a></p>
+        </div>
+      </div>
+      </div>
+    </section>
+    
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
