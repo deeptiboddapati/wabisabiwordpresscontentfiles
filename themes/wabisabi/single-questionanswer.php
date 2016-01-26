541b51d80331723fcad606adbd2781fb7e55ce89
Deepti Boddapati <deeptiboddapati@gmail.com>
2016-01-26 12:54:23 -0600

    before uploading to hosting

--- themes/wabisabi/single-questionanswer.php
+++ themes/wabisabi/single-questionanswer.php
@@ -34,10 +34,7 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 			</p>
 		
 	</div>
-	<div class="meta clearfix">
-	<div class="category"><?php echo get_the_category_list(); // Display the categories this post belongs to, as links ?></div>
-	<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); // Display the tags this post has, as links separated by spaces and pipes ?></div>
-</div><!-- Meta -->
+	
 </div>		
 
 
